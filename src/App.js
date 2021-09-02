576ee786fb87ed42b78d720e517436d1ac1e3b4b
shreyassanghvi <shreyas.sanghvi11@gmail.com>
2021-09-02 16:19:04 +0530

    Partial Sign in and Register Form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,62 @@ import './App.css';
 import {Component} from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
-import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import SignIn from "./Components/SignIn/SignIn";
 import Rank from "./Components/Rank/Rank";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
+import Register from "./Components/Register/Register";
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
+
+
+const app = new Clarifai.App({
+    apiKey: '96637bcd836e48379f22a03915c4b9b7'
+});
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: "",
+            imageURL: "",
+            box: {},
+            route: 'signin'
+        }
+    }
+
+
+    onInputChange = (event) => {
+
+        this.setState({input: event.target.value})
+
+    }
+    calculateFace = (data) => {
+        const clarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById("inputImage");
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifyFace.left_col * width,
+            topRow: clarifyFace.top_row * height,
+            rightCol: width - clarifyFace.right_col * width,
+            bottomRow: height - clarifyFace.bottom_row * height
+        }
+    }
+    displayFace = (box) => {
+        this.setState({box: box})
+    }
+    onSubmit = () => {
+        this.setState({imageURL: this.state.input})
+        console.log(this.state.imageURL)
+
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then((response) => this.displayFace(this.calculateFace(response))).catch(err => console.log(err))
+
+    }
+    onRouteChange = (route) => {
+        this.setState({route: route})
+    }
+
     render() {
         const particlesOption = {
             particles: {
@@ -19,17 +70,30 @@ class App extends Component {
                 }
             }
         };
+
         return (
             <div>
                 <Particles className={"particles"}
                            params={particlesOption}
                 />
                 <div className={"App"}>
-                    <Navigation/>
-                    <Logo/>
-                    <Rank/>
-                    <ImageLinkForm/>
-                    {/*  <FaceRecognition />*/}
+                    <Navigation onRouteChange={this.onRouteChange}/>
+                    {this.state.route === 'home' ?
+
+                        <div>
+                            <Logo/>
+                            <Rank/>
+                            <ImageLinkForm onInputChange={this.onInputChange} onSubmitButton={this.onSubmit}/>
+                            <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/>
+                        </div>
+                        : (
+                            this.state.route === 'signin' ?
+                                <SignIn onRouteChange={this.onRouteChange}/>
+                                :
+                                <Register onRouteChange={this.onRouteChange}/>
+
+                        )
+                    }
                 </div>
             </div>
         );
